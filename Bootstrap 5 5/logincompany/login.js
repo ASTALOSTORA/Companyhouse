ae0adcfe1882b8f7f38102cde87333ed9f27f53d
ASTALOSTORA <okamohamed2025@gmail.com>
2023-06-02 19:33:28 +0300

    created

--- Bootstrap 5 5/logincompany/login.js
+++ Bootstrap 5 5/logincompany/login.js
@@ -1,13 +1,24 @@
 let user = document.querySelector(".user")
 let company = document.querySelector(".company")
 let namecompany = document.querySelector(".name_company")
+let formuser = document.querySelector(".formuser")
+let formcompany = document.querySelector(".formcompany")
+let btn_login = document.querySelector(".btn_login")
+let input1_user = document.querySelector(".input_user")
+
+
+
 company.addEventListener("click",()=>{
   company.classList.add("bord")
   user.classList.remove("bord")
-  namecompany.style.display="block"
+  
+  formuser.style.display="none"
+  formcompany.style.display="block"
 })
 user.addEventListener("click",()=>{
   user.classList.add("bord")
   company.classList.remove("bord")
-  namecompany.style.display="none"
+  formuser.style.display="block"
+  formcompany.style.display="none"
 })
+
